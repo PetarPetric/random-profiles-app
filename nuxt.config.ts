7d2350a1c95eb86cd8b7a3266122487664c50464
PetarPetric <petricpetar22@gmail.com>
2025-10-20 13:40:59 +0200

    chore: tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/eslint']
-})
+  modules: ["@nuxt/image", "@nuxt/eslint", "@nuxtjs/tailwind"],
+});
