a45ad70d527795549f0b46dc94eef9ff8e488584
PetarPetric <petricpetar22@gmail.com>
2025-10-21 12:14:06 +0200

    feat: add modal to layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -62,7 +62,8 @@ useHead({
     <main class="transition-all duration-300 ease-in-out">
       <slot />
     </main>
-
+    <!-- Could have used teleport to main app -->
+    <ErrorMessageModal />
     <!-- Footer -->
     <footer class="bg-white border-t border-gray-200 mt-16">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
